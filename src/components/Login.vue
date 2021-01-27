91e652c68a95e046752058e0044835f7835dfe11
ZhouX <1719239641@qq.com>
2021-01-27 15:35:37 +0800

    03.绘制登录组件最基础的盒子

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,56 @@
 <template>
-  <div>
-    登录组件
+  <div class="login_container">
+    <!-- 登录盒子  -->
+    <div class="login_box"></div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b5b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './plugins/element.js';
 
-Vue.config.productionTip = false
+// 导入全局样式
+import './assets/css/global.css';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
