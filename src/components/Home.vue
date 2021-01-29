e476b1f35121c5a1cb8c3374cee6d92224c4fe6e
ZhouX <1719239641@qq.com>
2021-01-29 16:58:41 +0800

    21

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
       <el-aside :width="isCollapse ? '64px' : '200px'">
         <div class="toggle-button" @click="toggleCollapse">|||</div>
         <!-- 侧边栏菜单区域 -->
-        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409eff" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
           <!-- 一级菜单 -->
           <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单的模板区域 -->
@@ -25,7 +25,7 @@
               <span>{{ item.authName }}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id">
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path)">
               <template slot="title">
                 <!-- 图标 -->
                 <i class="el-icon-menu"></i>
@@ -58,11 +58,15 @@ export default {
         102: 'iconfont icon-danju',
         145: 'iconfont icon-baobiao'
       },
-      isCollapse: false
+      isCollapse: false,
+      // 被激活的链接地址
+      activePath: ''
     };
   },
   created() {
     this.getMenuList();
+    // 刷新的时候去 sessionStorage 里面去取
+    this.activePath = window.sessionStorage.getItem('activePath');
   },
   methods: {
     logout() {
@@ -78,6 +82,11 @@ export default {
     // 切换菜单的折叠与展开
     toggleCollapse() {
       this.isCollapse = !this.isCollapse;
+    },
+    saveNavState(activePath) {
+      // 保存链接的激活状态
+      window.sessionStorage.setItem('activePath', activePath);
+      this.activePath = activePath;
     }
   }
 };
