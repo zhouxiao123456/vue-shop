e1212161adb0c6395337c9a34286c0c9af8c7bda
ZhouX <1719239641@qq.com>
2021-01-29 14:43:37 +0800

    16

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ axios.defaults.baseURL = 'http://192.168.88.249:8888/api/private/v1/';
 
 //请求拦截器
 axios.interceptors.request.use((config) => {
-  config.headers.Authorization = window.sessionStorage.getItem('token,');
+  config.headers.Authorization = window.sessionStorage.getItem('token');
   return config;
 });
 
