1a1d1cf9fdd7d92253bd405d46b6df87e259ba49
ZhouX <1719239641@qq.com>
2021-01-29 14:31:40 +0800

    16

--- src/main.js
+++ src/main.js
@@ -11,6 +11,21 @@ import './assets/fonts/iconfont.css';
 
 import axios from 'axios';
 axios.defaults.baseURL = 'http://192.168.88.249:8888/api/private/v1/';
+
+//请求拦截器
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token,');
+  return config;
+});
+
+//响应拦截器
+axios.interceptors.response.use((res) => {
+  if (res.data.meta.msg === '无效token' && res.data.meta.status === 400) {
+    location.href = '/#/login';
+  }
+  return res;
+});
+
 Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
