3a90e6005b2a17b92bed7cede7ed0b1b82d3eb54
ZhouX <1719239641@qq.com>
2021-01-29 11:11:07 +0800

    10.配置全局Message全局弹框组件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -72,8 +72,8 @@ export default {
         //验证失败直接返回
         if (!vaild) return;
         const { data: res } = await this.$http.post('login', this.loginForm);
-        if (res.meta.status !== 200) return console.log('登录失败');
-        console.log('登录成功');
+        if (res.meta.status !== 200) return this.$message.error('登录失败');
+        this.$message.success('登录成功');
       });
     }
   }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
-import { Button, Form, FormItem, Input } from 'element-ui';
+import { Button, Form, FormItem, Input, Message } from 'element-ui';
 
 Vue.use(Button);
 Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
+
+//把弹框组件挂着了，这样每一个组件都可以直接通过this访问
+Vue.prototype.$message = Message;
