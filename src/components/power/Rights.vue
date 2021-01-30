4f51424b910b9b6500e203258df1e51d31e7ec19
ZhouX <1719239641@qq.com>
2021-01-30 16:54:36 +0800

    36

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -1,8 +1,38 @@
 <template>
-  <div>呀呼！</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      123
+    </el-card>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      //权限列表
+      rightsList: []
+    };
+  },
+  created() {
+    this.getRightsList();
+  },
+  methods: {
+    async getRightsList() {
+      const { data: res } = await this.$http.get('rights/list');
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限列表失败');
+      }
+      this.rightsList = res.data;
+    }
+  }
+};
 </script>
 
 <style lang="less"></style>
