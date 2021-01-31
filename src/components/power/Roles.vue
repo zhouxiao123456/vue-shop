7bddb52ca3e94c3b51825c013c481b37309f7dac
ZhouX <1719239641@qq.com>
2021-01-31 10:36:05 +0800

    40

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -19,13 +19,25 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <el-row v-for="item1 in scope.row.children" :key="item1.id">
+            <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
               <!-- 渲染一级权限 -->
               <el-col :span="5">
                 <el-tag>{{ item1.authName }}</el-tag>
+                <i class="el-icon-caret-right"></i>
               </el-col>
-              <!-- 渲染二级和三级权限 -->
-              <el-col :span="19"></el-col>
+              <el-col :span="19">
+                <!-- 渲染二级权限 -->
+                <el-row :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                  <el-col :span="6">
+                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <!-- 渲染三级权限 -->
+                    <el-tag v-for="item3 in item2.children" :key="item3.id">{{ item3.authName }}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
             </el-row>
           </template>
         </el-table-column>
@@ -69,4 +81,18 @@ export default {
 };
 </script>
 
-<style lang="less" scope></style>
+<style lang="less" scope>
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
+}
+</style>
